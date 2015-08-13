c8ec610451ba1f6f79d3c74567dea44533df3139
futurecash <futurecash@safe-mail.net>
2015-08-13 08:20:20 +0300

    Ticker changed to FCASH

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FRC");
-    case mBTC: return QString("mFRC");
-    case uBTC: return QString::fromUtf8("μFRC");
+    case BTC: return QString("FCASH");
+    case mBTC: return QString("mFCASH");
+    case uBTC: return QString::fromUtf8("μFCASH");
     default: return QString("???");
     }
 }
